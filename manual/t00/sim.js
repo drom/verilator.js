2c1e603e7220db97cba6b8807c3d06cb237a41da
drom <alex.drom@gmail.com>
2014-02-05 23:41:09 -0800

    live svg inside index.html

--- manual/t00/sim.js
+++ manual/t00/sim.js
@@ -8,16 +8,31 @@
 		var i, ilen;
 		ilen = list.length;
 		for (i = 0; i < ilen; i++) {
-			console.log('v_'+list[i]);
-			document.getElementById('v_'+list[i]).innerHTML = top[list[i]];
+			document.getElementById('v_'+list[i]).textContent = top[list[i]];
 		}
 	};
-
+	function render_all (top) {
+		render(top, ['a','v__DOT__aa','b','v__DOT__bb','c','clk']);
+	};
 	var i, main_time = 0, top = create(Vt00);
 	top.constructor();
 	// set some inputs
 	// while () {
-	render(top, ['a','v__DOT__aa','b','v__DOT__bb','c']);
+	top.clk = 0;
+	render_all(top);
+	document.onkeydown = function (e) {
+		if(e.keyCode != 32) {return;}
+		top.clk = 1;
+		top.eval();
+		render_all(top);
+	};
+	document.onkeyup = function (e) {
+		if(e.keyCode != 32) {return;}
+		top.clk = 0;
+		top.eval();
+		render_all(top);
+	};
+/*
 	for (i = 0; i < 100; i++) {
 		if ((main_time % 4) == 1) {
 			top.clk = 1; // Toggle clock
@@ -29,4 +44,5 @@
 		console.log(top);
 		main_time++;
 	}
+*/
 })();
--- manual/t00/verilated.js
+++ manual/t00/verilated.js
@@ -14,4 +14,4 @@ verilated.VL_SIG32 = function (msb,lsb) { var x = 0; return x|0 };
 
 verilated.VL_DEBUG_IF = function (fn) {};
 
-verilated.VL_RAND_RESET_I = function (num) { return (Math.round(Math.random()*(Math.pow(2,num)))|0)};
+verilated.VL_RAND_RESET_I = function (num) { return (Math.floor(Math.random()*(Math.pow(2,num)))|0)};
